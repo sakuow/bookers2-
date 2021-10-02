c21d96d3ba985486170fb38ba7b850214570acc2
sakuow <yusaku1335@gmail.com>
2021-10-02 09:15:22 +0000

    5 commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about'
   resources :users
-  resources :books, only: [:index, :create, :show, :edit, :destroy]
+  resources :books, only: [:index, :create, :show, :edit, :destroy, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
