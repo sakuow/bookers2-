2473f4ba19bfaec80ffd78fb888ca3fe9ed341cf
sakuow <yusaku1335@gmail.com>
2021-10-03 00:21:49 +0000

    7 commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+       flash[:notice] = "You have created book successfully."
        redirect_to book_path(@book.id)
     else
        @user = User.find(current_user.id)
@@ -26,11 +27,17 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+       render :edit
+    else
+       redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+       flash[:notice] = "You have updated book successfully."
        redirect_to book_path(@book.id)
     else
        render :edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,11 +14,17 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+       render "edit"
+    else
+       redirect_to user_path(current_user.id)
+    end
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+       flash[:notice] = "You have updated user successfully."
        redirect_to user_path(@user.id)
     else
        render :edit
